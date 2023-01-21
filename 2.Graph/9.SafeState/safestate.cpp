e4b42c32d5b84791c689ae7ca21b47a0c7faecc0
Depin S <depin.s@ivanti.com>
2023-01-22 00:01:30 +0530

    Safe State DFS

--- 2.Graph/9.SafeState/safestate.cpp
+++ 2.Graph/9.SafeState/safestate.cpp
@@ -14,6 +14,32 @@ using namespace std;
 
 class Solution
 {
+
+    	bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[],  int check[]) {
+		vis[node] = 1;
+		pathVis[node] = 1;
+		check[node] = 0;
+		// traverse for adjacent nodes
+		for (auto it : adj[node]) {
+			// when the node is not visited
+			if (!vis[it]) {
+			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
+					check[node] = 0;
+					return true;
+				}
+
+			}
+			// if the node has been previously visited
+			// but it has to be visited on the same path
+			else if (pathVis[it]) {
+				check[node] = 0;
+				return true;
+			}
+		}
+		check[node] = 1;
+		pathVis[node] = 0;
+		return false;
+	}
 public:
 
 
@@ -25,6 +51,26 @@ Extra O(N*logN) for sorting the safeNodes array, where N is the number of safe n
 Space Complexity: O(N) + O(N) + O(N) ~ O(3N), O(N) for the indegree array, O(N) for the queue data structure used in BFS(where N = no.of nodes), and extra O(N)
 for the adjacency list to store the graph in a reversed order.
 */
+
+
+
+
+	vector<int> eventualSafeNodesDFS(int V, vector<int> adj[]) {
+		int vis[V] = {0};
+		int pathVis[V] = {0};
+		int check[V] = {0};
+		vector<int> safeNodes;
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				dfsCheck(i, adj, vis, pathVis, check);
+			}
+		}
+		for (int i = 0; i < V; i++) {
+			if (check[i] == 1) safeNodes.push_back(i);
+		}
+		return safeNodes;
+	}
+
     vector<int> SafeNodeBFS(int V, vector<int> adj[]) {
 
         // Reverse the link;
